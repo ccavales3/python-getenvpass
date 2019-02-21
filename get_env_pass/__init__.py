0fd2f8c17e83838a8cac44efd6b409d181cb5337
Caesar Cavales <cbcavale@us.ibm.com>
2019-02-21 14:07:26 -0500

    added more test and clarified dicstrings

--- get_env_pass/__init__.py
+++ get_env_pass/__init__.py
@@ -1,11 +1,16 @@
 """
-get_env_pass module
+Contains utils and functions for python-getenvpass module
 """
 from getpass import getpass
 import os
 
-def get_env_pass(env_var):
+
+def get_env_pass(env_var=''):
     """
-    get_env_pass function
+    Retrieves environment variable. If does not exist, run getpass module
+
+    Returns:
+        Environment variable value or getpass.
+
     """
     return os.environ[env_var] if env_var in os.environ else getpass()
--- tests/get_env_pass/test_get_env_pass.py
+++ tests/get_env_pass/test_get_env_pass.py
@@ -3,7 +3,9 @@ This file unittests the get_env_pass/__init__.py with pytest.
 
 Methods:
     test_call_get_env_pass_with_var
-    test_call_get_env_pass_without_var
+    test_call_get_env_pass_with_var_doesnt_exist
+    test_call_get_env_pass_no_args
+    test_call_get_env_pass_more_args
 """
 import os
 import unittest
@@ -13,6 +15,7 @@ import pytest
 
 from get_env_pass import get_env_pass
 
+
 class TestInitSbt(unittest.TestCase):
     """
     Creating a basic test class in order to test get_env_pass/__init__.py script
@@ -20,7 +23,7 @@ class TestInitSbt(unittest.TestCase):
 
     def test_call_get_env_pass_with_var(self):
         """
-        Calling get_env_pass with a system variable
+        Should return environment value
         """
         os.environ['TEST_VAR'] = 'TestVar'
         sys_var = get_env_pass('TEST_VAR')
@@ -32,19 +35,28 @@ class TestInitSbt(unittest.TestCase):
     @patch('get_env_pass.getpass', return_value='does_not_exist')
     def test_call_get_env_pass_with_var_doesnt_exist(self, sys_var):
         """
-        test
+        Should return getpass value if environment variable does not exist
         """
         sys_var = get_env_pass('TEST_VAR')
 
         self.assertEqual(sys_var, 'does_not_exist')
 
-
-    def test_call_get_env_pass_without_var(self):
+    @patch('get_env_pass.getpass', return_value='no_args')
+    def test_call_get_env_pass_no_args(self, sys_var):
         """
-        test
+        Should return getpass value if no argument(s) is passed in get_env_pass
+        """
+        sys_var = get_env_pass()
+
+        self.assertEqual(sys_var, 'no_args')
+
+    def test_call_get_env_pass_more_args(self):
+        """
+        Should return appropriate error when more than required arguments are passed in
         """
         with pytest.raises(TypeError) as type_error:
-            sys_var = get_env_pass()  # pylint: disable=no-value-for-parameter, unused-variable
+            # pylint: disable=too-many-function-args, unused-variable
+            sys_var = get_env_pass('arg1', 'arg2')
 
-        self.assertEqual('get_env_pass() missing 1 required positional argument: \'env_var\'',
+        self.assertEqual('get_env_pass() takes from 0 to 1 positional arguments but 2 were given',
                          str(type_error.value))
